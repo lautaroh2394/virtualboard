474c9cf1bcb2e445a992a9d2d6d7e41fd24b1ce3
lautaroh2394 <lautarohaase_23@hotmail.com>
2021-10-23 20:47:14 -0300

    Add test module

---
+++ test/testData.js
@@ -0,0 +1,9 @@
+import {Pawn} from "../models/Pawn.js"
+
+export const testBoardData = {
+    pawns: [
+        new Pawn({ name:1 }),
+        new Pawn({ name:2 }),
+        new Pawn({ name:3 })
+    ]
+}
